1ecce0fa7cf8c211d3f4a3e3ca894754e84bec35
Guilherme Brasil Pintarelli <gp1500@gmail.com>
2016-08-05 09:53:14 -0300

    Update UART_OutUDec4.c

--- UART_OutUDec4.c
+++ UART_OutUDec4.c
@@ -1,17 +1,15 @@
-# Auto detect text files and perform LF normalization
-* text=auto
-
-# Custom for Visual Studio
-*.cs     diff=csharp
-
-# Standard to msysgit
-*.doc	 diff=astextplain
-*.DOC	 diff=astextplain
-*.docx diff=astextplain
-*.DOCX diff=astextplain
-*.dot  diff=astextplain
-*.DOT  diff=astextplain
-*.pdf  diff=astextplain
-*.PDF	 diff=astextplain
-*.rtf	 diff=astextplain
-*.RTF	 diff=astextplain
+// By GBPintarelli
+// 05/08/2016
+// Turn the decimal value from the ADC to individual chars (and send then true UART)
+// UARTCharPut Funciton: Sends  a char through UART
+			  
+void UART_OutUDec4(unsigned int n){		// n is the input 0 to 9999
+	UARTCharPut(UART0_BASE, 0x30+n/1000);	// hundreds digit
+	n = n%1000;								// 0 to 99
+	UARTCharPut(UART0_BASE, 0x30+n/100);	// hundreds digit
+	n = n%100;								// 0 to 99
+	UARTCharPut(UART0_BASE, 0x30+n/10);		// tens digit
+	n = n%10;								// 0 to 9
+	UARTCharPut(UART0_BASE, 0x30+n);		// ones digit
+	//UARTCharPut( UART0_BASE, '\r' );  //new line
+	//UARTCharPut( UART0_BASE, '\n' );
