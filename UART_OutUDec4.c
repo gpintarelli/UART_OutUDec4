2ab7ff8aa6a9bb94afd06ac0da5b6fb7b4382816
Guilherme Brasil Pintarelli <gp1500@gmail.com>
2016-08-05 09:54:59 -0300

    Update UART_OutUDec4.c

--- UART_OutUDec4.c
+++ UART_OutUDec4.c
@@ -5,11 +5,11 @@
 			  
 void UART_OutUDec4(unsigned int n){		// n is the input 0 to 9999
 	UARTCharPut(UART0_BASE, 0x30+n/1000);	// hundreds digit
-	n = n%1000;								// 0 to 99
+	n = n%1000;				// 0 to 99
 	UARTCharPut(UART0_BASE, 0x30+n/100);	// hundreds digit
-	n = n%100;								// 0 to 99
-	UARTCharPut(UART0_BASE, 0x30+n/10);		// tens digit
-	n = n%10;								// 0 to 9
-	UARTCharPut(UART0_BASE, 0x30+n);		// ones digit
+	n = n%100;				// 0 to 99
+	UARTCharPut(UART0_BASE, 0x30+n/10);	// tens digit
+	n = n%10;				// 0 to 9
+	UARTCharPut(UART0_BASE, 0x30+n);	// ones digit
 	//UARTCharPut( UART0_BASE, '\r' );  //new line
 	//UARTCharPut( UART0_BASE, '\n' );
