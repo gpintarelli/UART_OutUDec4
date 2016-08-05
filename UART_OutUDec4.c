734dbf202e2908c78697f27ec44340cbeb9980ee
Guilherme Brasil Pintarelli <gp1500@gmail.com>
2016-08-05 11:44:03 -0300

    Update UART_OutUDec4.c

--- UART_OutUDec4.c
+++ UART_OutUDec4.c
@@ -14,3 +14,19 @@ void UART_OutUDec4(unsigned int n){		// n is the input 0 to 9999
 	//UARTCharPut( UART0_BASE, '\r' );  //new line
 	//UARTCharPut( UART0_BASE, '\n' );
 }
+
+
+
+// Turn the decimal value to string
+
+unsigned char Data[5]; 				// 5-byte empty buffer
+void UDec2Str(unsigned short n){	// n is the input 0 to 9999
+	Data[0] = n/1000 + 0x30;		// tousands digit
+	n = n%1000;						// n is now between 0 and 999
+	Data[1] = n/100 + 0x30;			// hundreds digit
+	n = n%100;						// n is now between 0 and 99
+	Data[2] = n/10 + 0x30;			// tens digit
+	n = n%10;						// n is now between 0 and 9
+	Data[3] = n + 0x30;				// ones digit
+	Data[4] = 0;					// null termination
+}
